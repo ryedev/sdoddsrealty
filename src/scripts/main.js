2d23e34ac1c8420f9fbc8264f463186d3be4968f
Joe Tamburro <joetamburro38@gmail.com>
2014-10-22 07:51:08 -0400

    added smooth scroll and anchor links to sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,19 @@ $(document).ready(function(){
   $('#main-content-hl').fitText(2, { minFontSize: '16px', maxFontSize: '56px' });
   $('#main-content-sub-hl').fitText(2, { minFontSize: '10px', maxFontSize: '18px'});
   
+  // chris coyier's smooth scroll
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
   // thanks to james padolsey for help with this cross-browswer doc height
   $.getDocHeight = function(){
        var D = document;
